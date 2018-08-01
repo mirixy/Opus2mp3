a3d559446cd5d591104158c4a7dae644b91ffeaa
Joey <41879890+blackdever@users.noreply.github.com>
2018-08-01 12:54:56 +0200

    Update converter.py
    add shebang line for bash compatibility;
    add some blank lines to improve readability;

--- converter.py
+++ converter.py
@@ -1,11 +1,18 @@
+#! -*- coding: utf-8 -*-
 import os
+
+
 eingabe = ""
 eingabe = input("Please type in your Fileformat: ")
+print("\n" + ("-" * 50) + "\n")
+
 fileformat = eingabe
-convert2mp3 ='for a in ~/Music/*.' + eingabe + '; do  ffmpeg -i "$a" -qscale:a 0 "${a[@]/%' + eingabe + '/mp3}"; done'
+convert2mp3 ='#!/bin/bash\n\nfor a in ~/Music/*.' + eingabe + ';\ndo  ffmpeg -hide_banner -i "$a" -qscale:a 0 "${a[@]/%' + eingabe + '/mp3}";\ndone'
 
 script_out = open("conver2mp3.txt", 'w')
 script_out.write(convert2mp3)
 script_out.close()
+
 convert = "cp conver2mp3.txt convert2mp3.sh && sudo chmod +x convert2mp3.sh && ./convert2mp3.sh"
+
 os.system(convert)
